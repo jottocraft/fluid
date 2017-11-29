d968f0796e9000ca0da7d7d51e4401ade771b467
jottocraft <jotto3053@gmail.com>
2017-11-28 19:40:40 -0800

    Update

--- fluid.js
+++ fluid.js
@@ -1,4 +1,18 @@
 /*!
+Fluid Modules
+ */
+fluid = new Object;
+fluid.dark = function() {
+    $("body").toggleClass("dark");
+}
+
+
+
+
+
+
+
+/*!
  * Materialize v0.100.2 (http://materializecss.com)
  * Copyright 2014-2017 Materialize
  * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
