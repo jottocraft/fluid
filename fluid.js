f877356fcc139e7c942e3bb89e9e4696951a8d12
jottocraft <jack@loefflerfamily.com>
2018-04-21 19:01:36 -0700

    Testing b4

--- fluid.js
+++ fluid.js
@@ -1,11 +1,42 @@
 /*!
-Fluid JS Modules 2.0 p3
-by jottocraft
+Fluid JS Modules v2.0 Beta 4
+
+Copyright (c) 2017-2018 jottocraft
+
+Materialize components Copyright (c) 2014-2018 Materialize
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 
 fluid = new Object;
 fluid.dark = function() {
-    $("body").toggleClass("dark");
+  $("body").toggleClass("dark");
+  if ( $(".themeico").length ) {
+    if (fluid.isDark()) {
+      $(".themeico").text("brightness_low");
+    } else {
+      $(".themeico").text("brightness_high");
+    }
+  }
+}
+fluid.isDark = function() {
+  return $("body").hasClass("dark");
 }
 /* Loader auto initilization */
 setTimeout(function () {
